267429e2264ec7d7948d5a7922295749ceb174e8
Alan Oliveira <developerjs.alan@gmail.com>
2021-04-14 11:02:23 -0300

    add Level Up Modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { GetServerSideProps } from 'next'
+
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
@@ -6,9 +8,22 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
 import { CountdownProvider } from '../contexts/CountdownContext';
 import styles from '../styles/components/pages/Home.module.css'
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props) {
   return (
+
+    <ChallengesProvider 
+    level={props.level}
+    currentExperience={props.currentExperience}
+    challengesCompleted={props.challengesCompleted}
+    >
     <div className={styles.container}>
       <Head>
         <title>Início | move.it</title>
@@ -29,5 +44,19 @@ export default function Home() {
      </section>
     </CountdownProvider>
    </div>
+   </ChallengesProvider>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted),
+    }
+  }
 }
